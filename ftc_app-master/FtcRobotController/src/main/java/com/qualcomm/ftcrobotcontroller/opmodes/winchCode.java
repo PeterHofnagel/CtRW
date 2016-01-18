e0871e01f7d7ffecb1321dd2ccc99a84b5657acb
Jeremy <josang234@gmail.com>
2016-01-17 19:44:06 -0500

    V_1 Working winch code

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/winchCode.java
@@ -0,0 +1,177 @@
+/* Copyright (c) 2014 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * TeleOp Mode
+ * <p>
+ * Enables control of the robot via the gamepad
+ */
+public class winchCode extends OpMode {
+
+	/*
+	 *
+	 */
+	// TETRIX VALUES.
+
+
+
+    Servo winch;
+
+	double clipmin = 0;
+	double clipmax = 1;
+	int calls = 0;
+
+
+
+
+	/**
+	 * Constructor
+	 */
+	public winchCode() {
+
+	}
+
+	/*
+	 * Code to run when the op mode is first enabled goes here
+	 *
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+	 */
+	@Override
+	public void init() {
+
+
+		/*
+		 *
+		 */
+      winch = hardwareMap.servo.get("winch");
+		calls=0;
+		winch.setPosition(0.5);
+
+
+
+
+
+	}
+
+	/*
+	 * This method will be called repeatedly in a loop
+	 *
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+	 */
+	@Override
+	public void loop() {
+
+		/*
+		 * Gamepad 1
+		 *
+		 * Gamepad 1 controls the motors via the left stick, and it controls the
+		 * wrist/claw via the a,b, x, y buttons
+		 */
+
+		// throttle: left_stick_y ranges from -1 to 1, where -1 is full up, and
+		// 1 is full down
+		// direction: left_stick_x ranges from -1 to 1, where -1 is full left
+		// and 1 is full right
+		double throttlewinch = gamepad2.left_stick_y;
+		double winchpower = (throttlewinch/2) + .5;
+
+		winchpower = Range.clip(winchpower,clipmin,clipmax);
+		winch.setPosition(winchpower);
+		telemetry.addData("servo power: ",winchpower);
+		telemetry.addData("throttle: ",throttlewinch);
+
+
+		// clip the right/left values so that the values never exceed +/- 1
+
+
+
+		// write the values to the motors
+
+		calls++;
+		// update the position of the arm.
+
+
+	}
+
+	/*
+	 * Code to run when the op mode is first disabled goes here
+	 * 
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+	 */
+
+	public void stop()
+        {
+
+	}
+
+    	
+	/*
+	 * This method scales the joystick input so for low joystick values, the 
+	 * scaled value is less than linear.  This is to make it easier to drive
+	 * the robot more precisely at slower speeds.
+	 */
+	double scaleInput(double dVal)  {
+		double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+				0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+		
+		// get the corresponding index for the scaleInput array.
+		int index = (int) (dVal * 16.0);
+		
+		// index should be positive.
+		if (index < 0) {
+			index = -index;
+		}
+
+		// index cannot exceed size of array minus 1.
+		if (index > 16) {
+			index = 16;
+		}
+
+		// get value from the array.
+		double dScale = 0.0;
+		if (dVal < 0) {
+			dScale = -scaleArray[index];
+		} else {
+			dScale = scaleArray[index];
+		}
+
+		// return scaled value.
+		return dScale;
+	}
+
+}
